951de977c3197c41286eff3923d0c621def3fc95
julianocolli03 <jc7236352@gmail.com>
2022-06-29 14:28:07 -0300

    array

--- mainn.js
+++ mainn.js
@@ -1,14 +1,9 @@
 function saludar(){
-    let nombre = prompt('¿Cómo es tu nombre?')
-    if (nombre !== ""){
-    alert("Hola " + nombre )
-    }else if(nombre == ""){
-        alert("escribi tu nombre dale")
-        saludar()
-    }else{
-        alert("solo letras")
-        saludar()
-    }
+    let nombre;
+    do{
+        nombre=(prompt("Como es tu nombre"));
+    } while (!(isNaN)(nombre))
+    
 }
 saludar()
 
@@ -23,35 +18,56 @@ function repreguntar(){
         alert('Ingrese una opción valida')
         repreguntar()
     }
+
 }
 repreguntar()
-
-function resina (){
-    let recina =parseFloat (prompt("Cuanto queres gastar?"))
-    if(recina <=10){
-        alert ("No podes comprar nada raton")
-        resina()
-    }
-    else{
-        alert ("Ok, ahora te muestro los productos")
-    }
+function resinas(literal) {
+    this.nombre = literal.nombre;
+    this.precio   = literal.precio;
+    this.color  = literal.color;
 }
-resina()
-let impTotal = 0
+
+// let array = []
+// const producta = [{ nombre: "Resinachica", precio: 391, color: "Negra" },
+//                 { nombre: "Resinagrande", precio: 3222, color: "Blanca" }
+// ]
+// for(const nombre of producta){
+// console.log(nombre.producta)
+// }
+class Producto{
+    constructor(precio, color, cantidad){
+    this.elPrecio = precio;
+    this.elcolor= color;
+    this.volumen = cantidad;
+}
+}
+
+const RESINITA = new Producto(100, "Negra", "1Litro" );  
+console.log(RESINITA.elPrecio);
+console.log(RESINITA.elcolor);
+console.log(RESINITA.volumen);
+const RESINOTA = new Producto(400, "A eleccion", "4Litros" );
+console.log(RESINOTA.elPrecio);
+console.log(RESINOTA.elcolor);
+console.log(RESINOTA.volumen);
+
 function resinass(){
     let medioPago = (prompt(`Tenemos: 
-    \n 1-Resina de 10 litros
-    \n 2-Resina de 20 litros`))
-    if(medioPago == '1'){
-        alert('Ok, sale 200 pesos')
+    \n 1-Resina barata
+    \n 2-Resina cara`))
+    if(medioPago == "1"){
+        alert (resinita.elPrecio + resinita.elcolor + resinita.volumen)
         let precioResinachica = 200
         alert(`Usted va a pagar: $${precioResinachica} si esta de acuerdo toque en "Aceptar" y el pago se realiza al instante. No tenemos mas stock que una`)
     }else if (medioPago == '2' ){
-    alert('OK, sale 300 pesos')
-    let resinaGrande = 300
+        
+        let resinaGrande = 300
     let cuanttas = parseInt(prompt("cuantas quiere?"))
     let precioFinal = resinaGrande * cuanttas
     alert(`Usted va a pagar: $${precioFinal} si esta de acuerdo toque en "Aceptar" y el pago se realiza al instante`)
     }
+    else{
+        alert("Ingresa una de las opciones")
+        resinass()
+    }
 }resinass()
-    
