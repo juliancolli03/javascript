1c4b5e5443366a2e35e4f1d0c0bb622143813bed
juliancolli03 <jc7236352@gmail.com>
2022-07-13 15:38:09 -0300

    casicarrito?

--- prrr.js
+++ prrr.js
@@ -1,3 +1,4 @@
+let catalogo = []
 const LISTAPRODUCTOS = [{
     id: 1, nombre: "Resina pequeña", cantidad: "1 litro", precio: 200, foto:"resinachica.jpg"
 },
@@ -14,49 +15,6 @@ const LISTAPRODUCTOS = [{
     id: 5, nombre: "Tinte a eleccion", cantidad: "1 litro", precio: 500, foto:"l.jpg"
 },]
 
-class Carrito {
-    constructor(id) {
-        this.id = id;
-        this.productos = [];
-    }
-
-    calcularTotal() {
-        let total = 0;
-        for(let i = 0; i < this.productos.length; i++) {
-            total = total + this.productos[i].precioKilo;
-        }
-        return total;
-    }
-
-}
-
-function limpiarCarrito() {
-    let divCarrito = document.getElementById("carrito");
-    divCarrito.innerHTML = "";
-}
-
-function actualizarCarrito(carrito) {
-    let divCarrito = document.getElementById("carrito");
-    carrito.productos.forEach(producto => {
-        divCarrito.innerHTML += renderCard(producto);
-    })
-    divCarrito.innerHTML += `<h1>Precio Total: $ ${carrito.calcularTotal()}</h1>`
-}
-
-function renovarStorage() {
-    localStorage.removeItem("carrito"); 
-    localStorage.setItem("carrito", JSON.stringify(carrito));
-}
-
-window.addEventListener('DOMContentLoaded', (e) => {
-    let storage = JSON.parse(localStorage.getItem("carrito"));
-    let carritoGuardado = new Carrito(storage.id, storage.productos);
-    storage.productos.forEach(producto => {
-        carritoGuardado.productos.push(producto);
-    })
-    limpiarCarrito();
-    actualizarCarrito(carritoGuardado);
-});
 
 
 let meterEnHtml = document.getElementById("cards")
@@ -71,21 +29,71 @@ function renderCard(producto) {
             <h5 class="card-title">${producto.id}.${producto.nombre}</h5>
             <p class="card-text"> Cantidad: ${producto.cantidad}</p>
             <p class="card-text">$ ${producto.precio}</p>
-            <a href="#" class="btn btn-primary botonDeCompra" id="${producto.id}">Agregar al carrito</a>
+        <a href="#" class="btn btn-primary botonDeCompra" id="${producto.id}">Agregar al carrito</a>
         </div>
     `;
-    meterEnHtml.innerHTML+=cardRendered
+meterEnHtml.innerHTML+=cardRendered
 }
+// function hacercarrito(){
+// const SELECCIONARCARRITO = document.getElementById("carrrito")
+// let carito = []
+// let productoSeleccionado 
+// let todo = productoSeleccionado + SELECCIONARCARRITO
+// let todotodo = todo + carito
+// }
 
-let carrito = new Carrito(1);
-let botones = document.querySelectorAll(".botonDeCompra");
-let arrayDeBotones = Array.from(botones);
-arrayDeBotones.forEach(boton => {
-    boton.addEventListener("click", (e) => {
-        let productoSeleccionado = catalogoProductos.find(producto => producto.id == e.target.id);
-        carrito.productos.push(productoSeleccionado);
-        limpiarCarrito();
-        actualizarCarrito(carrito);
-        renovarStorage();
+let botoncito = document.querySelector("#botonDecompra")
+
+class Carrito {
+    constructor(id) {
+        this.id = id;
+        this.productos = []
+    }
+  
+    calcularTotal() {
+        let total = 0;
+        for(let i = 0; i < this.productos.length; i++) {
+            total = total + this.productos[i].precio
+        }
+        return total
+    }
+  }
+  
+  catalogo.push(LISTAPRODUCTOS)
+  console.log(catalogo)
+
+  function limpiarCarrito() {
+    LISTAPRODUCTOS.innerHTML = "";
+  }
+  
+  function actualizarCarrito(carrito) {
+    carrito.productos.forEach(producto => {
+      const row = document.createElement("tr")
+      row.innerHTML = 
+        `<td><img src="${producto.foto}" class="fotoCarrito"></td>
+          <td>${producto.nombre} <br> $${producto.precio}</td>`
+      listaProductos.appendChild(row)
     })
-});
+    listaProductos.innerHTML += `<p  class="total">Precio Total: $ ${carrito.calcularTotal()}</p>`
+  }
+
+  let carrito = new Carrito(1);
+let arrayDeBotones = Array.from(botoncito)
+arrayDeBotones.forEach(boton => {
+  boton.addEventListener("click", (e) => {
+    let productoElegido = catalogo.find(producto => producto.id == producto.precio)
+    carrito.productos.push(productoElegido)
+    limpiarCarrito()
+    actualizarCarrito(carrito)
+})
+})
+
+addEventListener('DOMContentLoaded',()=>{
+    const btnMenu = document.getElementById("carrrito")
+    if(btnMenu){
+        btnMenu.addEventListener('click',()=>{
+            const menuItems = document.getElementById('carrrito')
+            
+        })
+    }
+  })
